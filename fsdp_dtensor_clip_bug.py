155c73064145db8a3a381bafed12628f9a93abef
RishabhSaini <rishabhsaini01@gmail.com>
2025-04-25 00:58:56 -0400

    what is supposed to happen with dtensor+float

--- fsdp_dtensor_clip_bug.py
+++ fsdp_dtensor_clip_bug.py
@@ -2,6 +2,7 @@ import os
 import torch
 from torch.distributed import init_process_group, destroy_process_group
 from torch.distributed.tensor import init_device_mesh, Shard, distribute_tensor
+from torch.nn.utils.clip_grad import _get_total_norm
 
 def setup():
     init_process_group(backend="gloo")
@@ -29,19 +30,12 @@ def main():
     param = torch.nn.Parameter(dtensor)  # wrap DTensor as Parameter
     param.grad = dtensor.clone()         # manually set grad
 
-    total_norm = torch.nn.utils.clip_grad_norm_([param], max_norm=MAX_NORM)
-    clip_coef = MAX_NORM / (total_norm + EPSILON)
-    print(f"[Rank {rank}] Partial norm: {total_norm.item():.10f}, {total_norm.placements}, clip_coef: {clip_coef.item():.10f}, {clip_coef.placements}")
+    dTensorNorm = (_get_total_norm([dtensor]) + 1.0) / 1.0
+    print(f"[Rank {rank}] dTensorNorm: {dTensorNorm.item():.10f}, {dTensorNorm.placements}")
 
-    local_param = torch.nn.Parameter(global_tensor)  # wrap DTensor as Parameter
-    local_param.grad = global_tensor.clone()         # manually set grad
-
-    local_total_norm = torch.nn.utils.clip_grad_norm_([local_param], max_norm=MAX_NORM)
-    local_clip_coef = MAX_NORM / (local_total_norm + EPSILON)
-    epsScale = MAX_NORM / (clip_coef*EPSILON) - local_total_norm / EPSILON
-    
+    tensorNorm = _get_total_norm(global_tensor) + 1.0
     if rank == 0:
-        print(f"     Local total norm: {local_total_norm.item():.10f},                                            Local clip_coef: {local_clip_coef.item():.10f}, epsScale: {epsScale.item():.10f}")
+        print(f"TensorNorm: {tensorNorm.item():.10f}")
 
 if __name__ == "__main__":
     main()
